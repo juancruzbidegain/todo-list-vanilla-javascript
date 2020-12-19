1f99e78159efb5733a694bb21ce34cc8b2f77231
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2020-12-18 22:55:58 -0300

    add front

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log(1)
+const formulario = document.querySelector('#formulario')
+const listaTares = document.querySelector('#lista-tareas')
+const template = document.querySelector('#template')
+const fragment = document.createDocumentFragment()
+
+let tareas = {}
