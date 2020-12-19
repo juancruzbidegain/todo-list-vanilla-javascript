71e12b6ff3ee7bb34d4f70c0df538e2ef0d6aac5
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2020-12-19 01:26:57 -0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 const formulario = document.querySelector('#formulario')
 const listaTares = document.querySelector('#lista-tareas')
-const template = document.querySelector('#template')
+const template = document.querySelector('#template').content
 const fragment = document.createDocumentFragment()
 
-let tareas = {}
+let tareas = {
+
+}
+
+// console.log(Date.now())
+
+formulario.addEventListener('submit' , e => {
+    e.preventDefault()
+    let tarea = e.target[0].value
+    
+    agregarTarea(tarea)
+    
+})
+
+
+const agregarTarea = tarea =>{
+    if(tarea.trim() === ""){
+        console.log('Esta Vacio')
+        return
+    }
+
+    const tareaAObjeto = {
+        id:Date.now(),
+        texto:tarea,
+        estado:false
+    }
+    tareas[tareaAObjeto.id] = tareaAObjeto
+    console.log(tareas)
+    formulario.reset()
+    formulario[0].focus()
+
+    imprimitTareas()
+}
+
+const imprimitTareas = () => {
+    Object.values(tareas).forEach(t => {
+        const clone = template.cloneNode(true)
+        clone.querySelector('p').textContent = t.texto
+        fragment.appendChild(clone)
+    })
+    listaTares.appendChild(fragment)
+}
+ 
